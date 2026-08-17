8ef5f472357287996692a604950d9ac249bb7963
sanskritirana <sanskritikrana@gmail.com>
2026-08-17 23:58:07 +0530

    Hello World!.c

---
+++ HelloWorld!.c
@@ -0,0 +1,31 @@
+/* 
+Write a C program to print the message: Hello World!
+
+Input Format
+
+No input is required.
+
+Constraints
+
+N/A
+
+Output Format
+
+Single line printing exactly: Hello World!
+
+Sample Output 0
+
+Hello World!
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Hello World!");  
+    return 0;
+}
