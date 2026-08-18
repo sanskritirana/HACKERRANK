98134267cf1e39ebdcad460b61d9bbee5483595f
sanskritirana <sanskritikrana@gmail.com>
2026-08-18 23:32:21 +0530

    CharactersDistance.c

---
+++ Basics/CharactersDistance.c
@@ -0,0 +1,54 @@
+/*
+Write a program that takes two lowercase characters and prints how many letters apart they are in the alphabet.
+
+Input Format
+
+Two lowercase characters char1 and char2 separated by space.
+
+Constraints
+
+Input letters are lowercase a–z
+char1 always comes before char2 in alphabetical order
+Use ASCII arithmetic only
+Output Format
+
+Single line printing the result as:
+The distance between {char1} and {char2} is {value}
+
+Sample Input 0
+
+a c
+Sample Output 0
+
+The distance between a and c is 2
+Sample Input 1
+
+b f
+Sample Output 1
+
+The distance between b and f is 4
+Sample Input 2
+
+m n
+Sample Output 2
+
+The distance between m and n is 1
+  */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int chardist(char c1, char c2){
+    int cdist=c2-c1;
+    return cdist;
+}
+
+int main() {
+    int char1,char2,cdist;
+    scanf("%c %c", &char1, &char2);
+    cdist=chardist(char1,char2);
+    
+    printf("The distance between %c and %c is %d",char1,char2,cdist);
+}
