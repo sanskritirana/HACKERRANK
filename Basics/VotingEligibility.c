3f4b141c39026db40bb650dfca1a89be08a24fd6
Sanskriti Rana <sanskritikrana@gmail.com>
2026-08-20 12:14:29 +0530

    Voting Eligibility

---
+++ Basics/VotingEligibility.c
@@ -0,0 +1,57 @@
+/*
+You are given an integer age.
+Your task is to check whether the person is eligible to vote.
+
+A person is eligible to vote if:
+
+age ≥ 18
+Input Format
+
+Single integer input: {age}
+
+Constraints
+
+0 ≤ age ≤ 150
+Output Format
+
+You must print:
+
+"Eligible" or "Not Eligible"
+Sample Input 0
+
+20
+Sample Output 0
+
+Eligible
+Sample Input 1
+
+17
+Sample Output 1
+
+Not Eligible
+Sample Input 2
+
+18
+Sample Output 2
+
+Eligible
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int age;
+    scanf("%d", &age);
+    
+    if (age>=0 && age<=150){
+        if (age>=18){
+            printf("Eligible");
+        }
+        else{
+            printf("Not Eligible");
+        }
+    }
+}
