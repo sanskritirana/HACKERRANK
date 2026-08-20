9a8f71bf3b56a4f69853d9b5146ca1695a77e5fa
Sanskriti Rana <sanskritikrana@gmail.com>
2026-08-20 12:06:35 +0530

    Convert 24-Hour Time to 12-Hour Format

---
+++ Basics/Convert24-HourTimeTo12-HourFormat.c
@@ -0,0 +1,101 @@
+/*
+You are given two integers h and m, representing time in 24-hour format:
+
+h → Hour (0 to 23)
+m → Minutes (0 to 59)
+Your task is to convert this time into 12-hour format and print the result in the form:
+
+{hh}:{mm} AM/PM - where hh and mm are converted hours and minutes double digit formats
+Conversion Rules
+
+00:00 to 11:59 → AM
+12:00 to 23:59 → PM
+In 12-hour format:
+
+00 hours → 12 AM
+12 hours → 12 PM
+13–23 hours → 1-11 PM
+Minutes must always be printed as given (including leading zeros when necessary).
+
+Input Format
+
+Two space-separated integers: {h} {m}
+
+where
+
+h is the hour
+m is the minutes
+Constraints
+
+0 ≤ h ≤ 23
+0 ≤ m ≤ 59
+Output Format
+
+Print the converted time in 12-hour format as:
+
+{hh}:{mm} AM/PM , where hh and mm are converted hour and minute double digit formats
+Sample Input 0
+
+00 10
+Sample Output 0
+
+12:10 AM
+Sample Input 1
+
+15 45
+Sample Output 1
+
+03:45 PM
+Sample Input 2
+
+12 30
+Sample Output 2
+
+12:30 PM
+Sample Input 3
+
+9 45
+Sample Output 3
+
+09:45 AM
+*/
+
+#include <stdio.h>
+
+void ConvertedTime (int h, int m){
+    int ConvertedH, ConvertedM;
+    char* AmPm;
+
+    if (h >= 0 && h <= 23 && m >= 0 && m <= 59) {
+
+        if (h == 0) {
+            ConvertedH = 12;
+            AmPm = "AM";
+        }
+        else if (h > 0 && h < 12) {
+            ConvertedH = h;
+            AmPm = "AM";
+        }
+        else if (h == 12) {
+            ConvertedH = 12;
+            AmPm = "PM";
+        }
+        else {
+            ConvertedH = h - 12;
+            AmPm = "PM";
+        }
+
+        ConvertedM = m;
+
+        printf("%d:%02d %s", ConvertedH, ConvertedM, AmPm);
+    }
+}
+
+int main() {
+    int h, m;
+    scanf("%d %d", &h, &m);
+    
+    ConvertedTime(h, m);
+    
+    return 0;
+}
