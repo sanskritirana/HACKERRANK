a0cba8ba394bb0d123c2f8707cf2c6a2fef2e7dc
Sanskriti Rana <sanskritikrana@gmail.com>
2026-08-21 23:59:09 +0530

    Create CheckIfTwoNumbersHaveSameLastDigit.c

---
+++ If_Else/CheckIfTwoNumbersHaveSameLastDigit.c
@@ -0,0 +1,68 @@
+/*
+You are given two integers a and b.
+Your task is to determine whether they share the same last digit.
+
+Bonus Challenge: Try approach of not using modulo operator.
+
+Input Format
+
+Two space separated integers: {a} {b}
+
+Constraints
+
+0 ≤ a, b ≤ 10^9
+Output Format
+
+You must print: "YES" or "NO"
+
+Sample Input 0
+
+57 87
+Sample Output 0
+
+YES
+Sample Input 1
+
+123 456
+Sample Output 1
+
+NO
+Sample Input 2
+
+100 0
+Sample Output 2
+
+YES
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a ,&b);
+    
+    int lasta = (a-((a/10)*10));
+    int lastb = (b-((b/10)*10));
+        
+    
+    if(a >= 0 && b <= pow(10,9))
+    {
+        if(lasta == lastb)
+        {
+            printf("YES");
+        }
+        
+        else{
+        printf("NO");
+        }
+
+    }
+    
+    
+   
+    return 0;
+}
