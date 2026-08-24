fe37393de8581d7c2d0cfe2b870958281b471208
Sanskriti Rana <sanskritikrana@gmail.com>
2026-08-24 22:56:28 +0530

    Swap Two Numbers

---
+++ Basics/SwapTwoNumbers.c
@@ -0,0 +1,38 @@
+/*
+Write a program to swap two numbers.
+
+Input Format
+
+Two unique integers a and b
+
+Constraints
+
+-10^6 <= a <= 10^6
+-10^6 <= b <= 10^6
+Output Format
+
+Two lines printing the swapped values of a and b
+
+Sample Input 0
+
+10 5
+Sample Output 0
+
+a = 5
+b = 10
+Sample Input 1
+
+-5 6
+Sample Output 1
+
+a = 6
+b = -5
+*/
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    printf("a = %d\n",b);
+    printf("b = %d\n",a);
+}
